b81cd2cfe22b9c7658acd684e54984b162548687
Aphatheology <aphatheology@gmail.com>
2024-01-29 21:40:36 +0100

    getting started with webflux

---
+++ src/main/java/com/aphatheology/fileuploadwebflux/FileUploadWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.aphatheology.fileuploadwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileUploadWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aphatheology/fileuploadwebflux/FileUploadWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aphatheology.fileuploadwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
